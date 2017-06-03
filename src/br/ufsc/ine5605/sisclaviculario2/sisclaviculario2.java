68a90127e46107691762603af283ac2a51289b8c
all.ferreira <all.ferreira@hotmail.com>
2017-06-03 14:37:17 -0300

    Commits iniciais

---
+++ src/br/ufsc/ine5605/sisclaviculario2/sisclaviculario2.java
@@ -0,0 +1,16 @@
+package br.ufsc.ine5605.sisclaviculario2;
+
+/**
+ *
+ * @author Allan
+ */
+public class sisclaviculario2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
